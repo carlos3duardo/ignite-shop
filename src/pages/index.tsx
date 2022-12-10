97ea1588cb2d9f148a1b781b25a3ca59c9026456
Carlos Eduardo <carloseduardo@cenarioweb.com.br>
2022-12-10 20:15:14 -0300

    Removendo arquivos desnecessários

---
+++ src/pages/index.tsx
@@ -0,0 +1,20 @@
+import Head from 'next/head'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Ignite Shop</title>
+        <link rel="icon" type="image/png" href="/favicon.png" />
+      </Head>
+
+      <main>
+        <h1>
+          Ignite Shop
+        </h1>
+
+      </main>
+    </div>
+  )
+}
