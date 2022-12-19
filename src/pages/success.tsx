0b568d41591dacc4a406585c3d0f1ca2d483ca4a
Carlos Eduardo <carloseduardo@cenarioweb.com.br>
2022-12-18 21:06:38 -0300

    Estilizando a página de sucesso

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,3 +1,21 @@
+import Link from 'next/link';
+import { ImageContainer, SuccessContainer } from '../styles/pages/success';
+
 export default function Product() {
-  return <h1>Success</h1>;
+  return (
+    <SuccessContainer>
+      <h1>Compra efetuada</h1>
+
+      <ImageContainer>
+        <img src="" alt="" />
+      </ImageContainer>
+
+      <p>
+        Uhuu <strong>Nome do usuário</strong>, sua{' '}
+        <strong>Nome do produto</strong> já está a caminho de sua casa
+      </p>
+
+      <Link href="/">Voltar ao catálogo</Link>
+    </SuccessContainer>
+  );
 }
