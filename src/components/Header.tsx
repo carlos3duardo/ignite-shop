5d9f53ce0311a1bea80cc93b0430870c8d7f56b2
Carlos Eduardo <carloseduardo@cenarioweb.com.br>
2022-12-22 10:09:05 -0300

    Criando área para exibir sacola de compras

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,27 +1,61 @@
 import Image from 'next/image';
 import Link from 'next/link';
-import { HeaderContainer, ShoppingCart } from '../styles/components/header';
+import {
+  CartItem,
+  CartList,
+  HeaderContainer,
+  ShippingCartContent,
+  ShoppingCart,
+} from '../styles/components/header';
 
 import logoImg from '../assets/logo.svg';
-import { Handbag } from 'phosphor-react';
+import { Handbag, X } from 'phosphor-react';
 import { useShoppingCart } from 'use-shopping-cart';
+import { useState } from 'react';
 
 export function Header() {
+  const [cartContentIsOpen, setCartContentIsOpen] = useState(false);
+
   const { cartDetails } = useShoppingCart();
 
-  const itemsCount = Object.values(cartDetails ?? {}).length;
+  const cartItems = Object.values(cartDetails ?? {});
+
+  console.log(cartItems);
+
+  function handleOpenCart() {
+    setCartContentIsOpen(true);
+  }
+
+  function handleCloseCart() {
+    setCartContentIsOpen(false);
+  }
 
   return (
-    <HeaderContainer>
-      <Link href="/">
-        <Image src={logoImg} alt="Logotipo da loja" />
-      </Link>
-      <Link href="/cart">
-        <ShoppingCart full={!!itemsCount}>
+    <>
+      <HeaderContainer>
+        <Link href="/">
+          <Image src={logoImg} alt="Logotipo da loja" />
+        </Link>
+        <ShoppingCart full={!!cartItems.length} onClick={handleOpenCart}>
           <Handbag size={24} weight="bold" />
-          {!!itemsCount && <span className="badge">{itemsCount}</span>}
+          {!!cartItems.length && (
+            <span className="badge">{cartItems.length}</span>
+          )}
         </ShoppingCart>
-      </Link>
-    </HeaderContainer>
+      </HeaderContainer>
+
+      <ShippingCartContent isOpen={cartContentIsOpen}>
+        <button className="btnClose" onClick={handleCloseCart}>
+          <X size={16} weight="bold" />
+        </button>
+        <h2>Sacola de compras</h2>
+
+        <CartList>
+          <CartItem>x</CartItem>
+          <CartItem>y</CartItem>
+          <CartItem>z</CartItem>
+        </CartList>
+      </ShippingCartContent>
+    </>
   );
 }
--- src/styles/components/header.ts
+++ src/styles/components/header.ts
@@ -11,7 +11,7 @@ export const HeaderContainer = styled('header', {
   justifyContent: 'space-between',
 });
 
-export const ShoppingCart = styled('div', {
+export const ShoppingCart = styled('button', {
   variants: {
     full: {
       true: {
@@ -20,6 +20,7 @@ export const ShoppingCart = styled('div', {
     },
   },
 
+  border: 0,
   backgroundColor: '$gray800',
   color: '$gray500',
   width: 48,
@@ -31,6 +32,8 @@ export const ShoppingCart = styled('div', {
   justifyContent: 'center',
 
   position: 'relative',
+
+  cursor: 'pointer',
 
   '.badge': {
     position: 'absolute',
@@ -50,3 +53,59 @@ export const ShoppingCart = styled('div', {
     borderRadius: 12,
   },
 });
+
+export const ShippingCartContent = styled('div', {
+  variants: {
+    isOpen: {
+      true: {
+        transform: 'translateY(0%)',
+        opacity: 1,
+      },
+    },
+  },
+
+  position: 'fixed',
+  top: 0,
+  right: 0,
+  bottom: 0,
+  zIndex: 10,
+  backgroundColor: '$gray800',
+  minWidth: 480,
+  boxShadow: '-4px 0px 30px rgba(0, 0, 0, 0.8);',
+
+  transform: 'translateX(110%)',
+  opacity: 0,
+  transition: 'all 0.4s ease',
+
+  padding: '4.5rem 3rem 3rem 3rem',
+
+  '.btnClose': {
+    border: 0,
+    width: 24,
+    height: 24,
+    backgroundColor: 'transparent',
+    color: '$gray300',
+
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+
+    position: 'absolute',
+    top: 28,
+    right: 28,
+
+    cursor: 'pointer',
+  },
+
+  h2: {
+    fontSize: '$lg',
+  },
+});
+
+export const CartList = styled('ul', {
+  margin: '2rem 0 0 0',
+  padding: 0,
+  listStyle: 'none',
+});
+
+export const CartItem = styled('li', {});
